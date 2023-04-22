1956657a10bb0d45d39390495c67800ac0301ff6
Razor-eng <83156526+Razor-eng@users.noreply.github.com>
2023-04-23 02:19:56 +0530

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Card from "../components/Card";
+import Navbar from "../components/Navbar";
+// import Sidebar from "../components/Sidebar";
+import Spinner from "../components/Spinner";
+import { clearVideos } from "../store";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { getHomePageVideos } from "../store/reducers/getHomePageVideos";
+import { HomePageVideos } from "../Types";
+
+export default function Home() {
+  const dispatch = useAppDispatch();
+  const videos = useAppSelector((state) => state.youtubeApp.videos);
+
+  useEffect(() => {
+    return () => {
+      dispatch(clearVideos());
+    };
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(getHomePageVideos(false));
+  }, [dispatch]);
+
+  return (
+    <div className="max-h-screen">
+      <div style={{ height: "7.5vh" }}>
+        <Navbar />
+      </div>
+      <div className="flex" style={{ height: "92.5vh" }}>
+        {/* <Sidebar /> */}
+        {videos.length ? (
+          <InfiniteScroll
+            dataLength={videos.length}
+            next={() => dispatch(getHomePageVideos(true))}
+            hasMore={videos.length < 500}
+            loader={<Spinner />}
+            height={0}
+          >
+            <div className="flex flex-wrap items-center justify-evenly sm:p-8 py-5">
+              {videos.map((item: HomePageVideos) => {
+                return <Card data={item} key={item.videoId} />;
+              })}
+            </div>
+          </InfiniteScroll>
+        ) : (
+          <Spinner />
+        )}
+      </div>
+    </div>
+  );
+}
